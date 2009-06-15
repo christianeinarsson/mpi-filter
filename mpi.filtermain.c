5e62aa64a399062eaa08d23fa709fb646ab723a3
Christian Ejnarsson <x_chrej@neolith1.(none)>
2009-06-15 17:11:21 +0200

    comments done on mpi.filtermain.c

--- mpi.filtermain.c
+++ mpi.filtermain.c
@@ -15,7 +15,9 @@
 int msglevel = 100;
 
 int main (int argc, char * argv[]) {
-
+	/*
+	 * Init itac tracing
+	 * */
 	VT_initialize(&argc, &argv);
 	int func_statehandle;
 	VT_funcdef("Main", VT_NOCLASS, &func_statehandle);
@@ -23,12 +25,9 @@ int main (int argc, char * argv[]) {
 
 	/*
 	* Init MPI
-	*
 	**/
-
 	int np, me;
 	int ierr = MPI_Init(&argc, &argv);
-
 	MPI_Comm_size(MPI_COMM_WORLD, &np);
 	MPI_Comm_rank(MPI_COMM_WORLD, &me);
 
@@ -44,29 +43,27 @@ int main (int argc, char * argv[]) {
 	MPI_Address(&item.r, &displ[0]);
 	MPI_Address(&item.g, &displ[1]);
 	MPI_Address(&item.b, &displ[2]);
-	displ[0] -= start; // Displacement relative to address of start
-	displ[1] -= start; // Displacement relative to address of start
-	displ[2] -= start; // Displacement relative to address of start
+	 // Displacements relative to address of start
+	displ[0] -= start;
+	displ[1] -= start;
+	displ[2] -= start;
 	MPI_Type_struct(3, block_lengths, displ, block_types, &MPI_PIXEL);
 	MPI_Type_commit(&MPI_PIXEL);
 
 	/*
 	* Program start
-	*
 	* */
-
-	int radius = MAX_RAD + 10;
+	int radius = MAX_RAD + 1;
 	int xsize, ysize;
 
 	int * blockdata = malloc((BLOCKDATA_SIZE) * sizeof(int));
 	pixel *src;
-
+	double weight[radius];
 
 	pmesg(50, "Process %d of %d\n",me,np);
 
 	/*
 	* Take care of the arguments
-	*
 	* */
 	if ((me == 0) && (argc != 4)) {
 		fprintf(stderr, "Usage: %s radius infile outfile (radius = 0 -> threshold otherwise blur)\n", argv[0]);
@@ -84,7 +81,6 @@ int main (int argc, char * argv[]) {
 			MPI_Finalize();
 			exit(1);
 		}
-
 		if(radius == 0){
 			pmesg(50, " *** Doing threshold filtering *** \n");
 		}else{
@@ -92,14 +88,18 @@ int main (int argc, char * argv[]) {
 		}
 	}
 
-	double weight[radius];
-
+	/*
+	 * If we are going to do blur filteringen let process 1 calculate the weightfunction
+	 * */
 	if((me == 1) && (radius > 0)){
 		pmesg(50, "Process %d: Calculating weight function.\n", me);
 		get_gauss_weights(radius, weight);
 		pmesg(50, "Process %d: Calculating weight function. Done!\n", me);
 	}
 
+	/*
+	 * Let process zero alocate memmory and read the picture from disk
+	 * */
 	if(me == 0){
 
 		src = (pixel *) malloc(MAX_PIXELS * sizeof(pixel));
@@ -122,93 +122,148 @@ int main (int argc, char * argv[]) {
 
 		pmesg(50, "Process %d: Partitioning on %d processes done.\n", me, np);
 
-
 	}
+
+	/*
+	 * Do a broadcast of the partitioning data.
+	 * */
 	if(me==0)pmesg(50, "blockdata.xsize=%i\n",blockdata[X_SIZE]);
-
 	if(me==0)pmesg(50, "will bcast blockdata\n");
+
 	MPI_Bcast(&blockdata[0], BLOCKDATA_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-	if(me==0)pmesg(50, "will bcast blockdata\n");
 
+	if(me==0)pmesg(50, "will bcast blockdata\n");
 
 	pixel *local_src;
-
-	if(radius > 0 ){  // Do Blur filtering
+	/*
+	 * Do blur filtering1
+	 * */
+	if(radius > 0 ){
+		/*
+		 * setup tracing of blur filter
+		 * */
 		int blur_symdef;
 		VT_funcdef("Filter:Blur", VT_NOCLASS, &blur_symdef);
 		VT_enter(blur_symdef, VT_NOSCL);
+
+		/*
+		 * Let everyone alocate enough memmory for there task.
+		 * */
 		if(me==0)pmesg(50, "will allocate local_src\n");
 		local_src = (pixel *) malloc(blockdata[SCATTER_SIZE + me]*sizeof(pixel));
+
+		/*
+		 * Broadcast the weighted array.
+		 * */
 		if(me==0)pmesg(50, "will bcast weight\n");
-
 		MPI_Bcast(weight, radius, MPI_DOUBLE, 1, MPI_COMM_WORLD);
-		if(me==0)pmesg(50, "done bcast weight, will scatterv picture\n");
+		if(me==0)pmesg(50, "done bcast weight\n");
 
+		/*
+		 * setup tracing of the scatter operation
+		 * */
 		int scatterv_blur_symdef;
 		VT_funcdef("Filter:Blur:ScatterV", VT_NOCLASS, &scatterv_blur_symdef);
 		VT_enter(scatterv_blur_symdef, VT_NOSCL);
+		/*
+		 * Scatter the image to each process local_src
+		 * */
+		if(me==0)pmesg(50, "will scatterv picture\n");
 		MPI_Scatterv(src, &blockdata[SCATTER_SIZE], &blockdata[SCATTER_OFFSET], MPI_PIXEL,
-			local_src, blockdata[SCATTER_SIZE + me], MPI_PIXEL,
-			0, MPI_COMM_WORLD);
+						local_src, blockdata[SCATTER_SIZE + me], MPI_PIXEL,
+						0, MPI_COMM_WORLD);
 		VT_leave(VT_NOSCL);
-
 		if(me==0)pmesg(50, "done scatterv picture\n");
 
+		/*
+		 * Read vital data for the blur funtion from the partition data in blockdata
+		 * */
 		int local_y_offset =  blockdata[GATHER_LOCAL_OFFSET + me ] / blockdata[X_SIZE];
 		int local_y_size = blockdata[SCATTER_SIZE + me] / blockdata[X_SIZE];
 		int local_y_stop = (blockdata[GATHER_LOCAL_OFFSET + me] + blockdata[GATHER_SIZE + me]) / blockdata[X_SIZE];
 
-		//each process blurs it's part of the image
-
+		/*
+		 * Each process blurs it's part of the image
+		 * */
 		pmesg(50, "Process %d: Calling blurfilter.\n",me);
-
 		blurfilter(local_y_offset, blockdata[X_SIZE], local_y_size, local_src, radius, weight, local_y_stop);
-
 		pmesg(50, "Process %d: Filtering done!\n", me);
+
 		VT_leave(VT_NOSCL);
-	}else{ // Do threshold filtering
+	}else{
+		/*
+		 * Do threshold filtering
+		 * */
+		/*
+		 * Setup tracing of the threshold filter
+		 * */
 		int threshold_symdef;
 		VT_funcdef("Filter:Threshold", VT_NOCLASS, &threshold_symdef);
 		VT_enter(threshold_symdef, VT_NOSCL);
 
+		/*
+		 * Alocate memmory for local picture buffer
+		 * */
 		local_src = (pixel *) malloc(blockdata[GATHER_SIZE + me]*sizeof(pixel));
 
+		/*
+		 * Setup tracing of the scatterv operation
+		 * */
 		int scatterv_threshold_symdef;
 		VT_funcdef("Filter:Threshold:ScatterV", VT_NOCLASS, &scatterv_threshold_symdef);
 		VT_enter(scatterv_threshold_symdef, VT_NOSCL);
+
+		/*
+		 * Do the scatterv operation
+		 * */
 		MPI_Scatterv(src, &blockdata[GATHER_SIZE], &blockdata[GATHER_GLOBAL_OFFSET], MPI_PIXEL,
 			local_src, blockdata[GATHER_SIZE + me], MPI_PIXEL,
 			0, MPI_COMM_WORLD);
 		VT_leave(VT_NOSCL);
-		// Calculate  local threshold value
 
+		/*
+		 * Calculate  local threshold value
+		 * */
 		int my_threshold = threshold(blockdata[GATHER_SIZE + me], local_src);
 		pmesg(50, "Process %d: my_threshold=%d\n",me, my_threshold);
 
 
-		// Collect and calculate and distribute global theshold value
-
+		/*
+		 * Collect the local theshold values with MPI_Allreduce sum.
+		 * */
 		int sum_threshold;
 		MPI_Allreduce( &my_threshold, &sum_threshold, 1, MPI_INT,MPI_SUM,MPI_COMM_WORLD);
+
+		/*
+		 * Calculate the global threshold value (done by everyone in parallel, thanks to mpi_ALLreduce :-))
+		 * */
 		sum_threshold /= np;
 
-		// Apply threshold filter
+		/*
+		 * Apply threshold filter
+		 * */
 		thresfilter(blockdata[GATHER_SIZE + me],sum_threshold,local_src);
 		VT_leave(VT_NOSCL);
 	}
 
-	//proc 0 recievs and writes the output
-
+	/*
+	 * When the filtering is done gather all results in process ones src buffer
+	 * */
 	MPI_Gatherv(&local_src[blockdata[GATHER_LOCAL_OFFSET +  me]], blockdata[GATHER_SIZE + me], MPI_PIXEL,
 		src, &blockdata[GATHER_SIZE], &blockdata[GATHER_GLOBAL_OFFSET], MPI_PIXEL,
 		0, MPI_COMM_WORLD);
 
-
+	/*
+	 * Let process one write the result to disk.
+	 * */
 	if((me==0)&&(write_ppm (argv[3], xsize, ysize, (char *)src) != 0)){
 		MPI_Finalize();
 		exit(1);
 	}
 
+	/*
+	 * Close down MPI and tracing.
+	 * */
 	MPI_Finalize();
 
 	VT_leave(VT_NOSCL);
